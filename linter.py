37ad9af74d100e4db41810f2854c89364918e06f
Christopher Pickering <cpickering@rhc.net>
2021-06-25 09:03:17 -0500

    updated regex

--- linter.py
+++ linter.py
@@ -4,7 +4,7 @@ from SublimeLinter.lint import PythonLinter
 class Radon(PythonLinter):
     cmd = 'radon cc ${file_on_disk} -s -n B --total-average'
     regex = (
-        r'^(?P<type>[a-zA-z])\s(?P<line>\d+):(?P<col>\d+).+-\s(?P<code>[a-zA-Z])\s\((?P<message>\d+)'
+        r'^\s+(?P<type>[a-zA-z])\s(?P<line>\d+):(?P<col>\d+).+-\s(?P<code>[a-zA-Z])\s\((?P<message>\d+)'
     )
     multiline = True
     defaults = {
