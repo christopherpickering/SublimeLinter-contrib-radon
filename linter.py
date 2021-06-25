f95e8f9faf98b20ac563c9d0e005e0eca1e8753e
Christopher Pickering <cpickering@rhc.net>
2021-06-25 09:35:06 -0500

    added missing sign

--- linter.py
+++ linter.py
@@ -38,6 +38,6 @@ def split_match(self, match):
 
         if match:
 
-            message = "This %s had a Radon cyclomatic complexity rank of %s, and scored %s." % (type_code[match.group('type')], code, message,  message_code[message])
+            message = "This %s had a Radon cyclomatic complexity rank of %s, and scored %s." % (type_code[match.group('type')], match.group('code'), message,  message_code[message])
 
         return match, line, col, error, warning, message, near
