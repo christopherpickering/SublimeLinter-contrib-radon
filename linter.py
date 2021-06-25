03ed1e832e04d48e79f9487175d9f3f1bf71c8ef
Christopher Pickering <cpickering@rhc.net>
2021-06-25 08:47:39 -0500

    changed to radon linter

--- linter.py
+++ linter.py
@@ -1,10 +1,12 @@
-from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+from SublimeLinter.lint import PythonLinter
 
 
-class __class__(Linter):
-    cmd = '__cmd__'
-    regex = r''
-    multiline = False
+class Radon(PythonLinter):
+    cmd = 'radon cc ${file_on_disk} -s -n B --total-average'
+    regex = (
+        r'^(?P<type>[a-zA-z])\s(?P<line>\d+):(?P<col>\d+).+-\s(?P<code>[a-zA-Z])\s\((?P<message>\d+)'
+    )
+    multiline = True
     defaults = {
         'selector': 'source.python'
     }
