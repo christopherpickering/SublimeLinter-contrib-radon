be8a9d8586210cb5d4116842a105a3dc136c83b6
Christopher Pickering <cpickering@rhc.net>
2021-06-25 09:41:51 -0500

    fixed indent

--- linter.py
+++ linter.py
@@ -1,17 +1,6 @@
 from SublimeLinter.lint import PythonLinter
-
-
-class Radon(PythonLinter):
-    cmd = 'radon cc ${file_on_disk} -s -n B'
-    regex = (
-        r'^\s+(?P<type>[a-zA-z])\s'
-        r'(?P<line>\d+):(?P<col>\d+).+-\s'
-        r'(?P<code>[a-zA-Z])\s(?P<warning>\()(?P<message>\d+)'
-    )
-    multiline = True
-    defaults = {
-        'selector': 'source.python',
-    }
+import logging
+logger = logging.getLogger('SublimeLinter.plugins.radon')
 
 
 type_code = {
@@ -29,13 +18,28 @@ message_code = {
     "F": "F (41+) very high - error-prone, unstable block.",
 }
 
-def split_match(self, match):
+
+class Radon(PythonLinter):
+    cmd = 'radon cc ${file_on_disk} -s -n B'
+    regex = (
+        r'^\s+(?P<type>[a-zA-z])\s'
+        r'(?P<line>\d+):(?P<col>\d+).+-\s'
+        r'(?P<code>[a-zA-Z])\s(?P<warning>\()(?P<message>\d+)'
+    )
+    multiline = True
+    defaults = {
+        'selector': 'source.python',
+    }
+
+    
+    def split_match(self, match):
         """
         Return the components of the error message.
         We override this to customize the message.
         """
         match, line, col, error, warning, message, near = super().split_match(match)
-
+        logger.error(match, line, col, error, warning, message, near)
+        logger.error(super().split_match(match))
         if match:
 
             message = "This %s had a Radon cyclomatic complexity rank of %s, and scored %s." % (type_code[match.group('type')], match.group('code'), message,  message_code[message])
